fb4ee14bc6b32d783f3437f381b762f8a3686651
Henrique França <henriqsf@gmail.com>
2022-10-27 16:19:46 -0300

    Componente com Propriedade

--- src/conponents/basicos/Primeiro.jsx
+++ src/conponents/basicos/Primeiro.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Primeiro(){
 
-    //qunado for variavel tem sempre que estar dentro de { }
+    //qunado for variavel tem sempre que estar dentro de um par de chaves { }
     const msg =  'Seja bem vindo!';
 
     return( 
         <div>
             <h2>Primeiro Componente</h2>
             <p>{ msg }</p>
-        </div>
+        </div>  
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import Primeiro from './conponents/basicos/Primeiro';
+import ComParametro from './conponents/basicos/ComParametro';
 
 //contate de um elemento pelo ID
 //const el =document.getElementById('root');
-
-
+//<ComParametro titulo='Segundo Componente' subtitulo='Muito legal'/>
 
 ReactDOM.render(
     //sintaxe jsx(parece html mas não é)
    
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
+        <ComParametro 
+        titulo='Segundo Componente!' 
+        aluno='Pedro' nota='9.3'/>
+
     </div>, 
     document.getElementById('root')
 );
