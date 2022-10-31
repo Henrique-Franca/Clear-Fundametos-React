d69bb8453dd07b504e183d55aeec91f742a82259
Henrique França <henriqsf@gmail.com>
2022-10-31 11:01:47 -0300

    Componente Card #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Cards from './conponents/layout/Cards'
 import Primeiro from './conponents/basicos/Primeiro';
 import ComParametro from './conponents/basicos/ComParametro';
 import Fragmento from './conponents/basicos/Fragment';
@@ -11,11 +12,20 @@ export default _ =>
     //precia sempre estar envolvido por uma tag como o <div>
     <div id='app'>
         <h1>Fundamentos React </h1>
+
+        <Cards titulo="#03Fragmento">
         <Fragmento/>
+        </Cards>
+
+        <Cards titulo="#02 Segundo Componente">
         <ComParametro 
         titulo='Segundo Componente!' 
         aluno='Pedro' nota={9.3}/>
-        <Primeiro></Primeiro>
+        </Cards>
+
+        <Cards titulo="#01 Primeiro Componente">
+        <Fragmento/>
+        </Cards>
     </div>
     
 
